35474cfd89ead4a6ecb424803089323f78640fc3
lewis-lea <68199449+lewis-lea@users.noreply.github.com>
2026-08-23 20:53:15 +0100

    Expose TimeSeries model

--- src/agentic_data_pipeline/__init__.py
+++ src/agentic_data_pipeline/__init__.py
@@ -1,5 +1,5 @@
 """Tools for ingesting and standardizing time-series data."""
 
-from agentic_data_pipeline.types import MarketQuote, TimeSeriesPoint
+from agentic_data_pipeline.types import MarketQuote, TimeSeries, TimeSeriesPoint
 
-__all__ = ["MarketQuote", "TimeSeriesPoint"]
+__all__ = ["MarketQuote", "TimeSeries", "TimeSeriesPoint"]
