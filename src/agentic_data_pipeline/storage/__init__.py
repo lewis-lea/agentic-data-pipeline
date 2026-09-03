f96ca0cac50013ae90bc6c18049d5c5bc612eac3
lewis-lea <68199449+lewis-lea@users.noreply.github.com>
2026-09-03 22:40:14 +0100

    Add persistence storage package

---
+++ src/agentic_data_pipeline/storage/__init__.py
@@ -0,0 +1,5 @@
+"""Persistence helpers for market and qualitative datasets."""
+
+from agentic_data_pipeline.storage.parquet import DatasetKey, ParquetStorage
+
+__all__ = ["DatasetKey", "ParquetStorage"]
